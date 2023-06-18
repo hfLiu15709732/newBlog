5efac9b978c59d810160500f540148049a0498d2
hfLiuX <11685497+hfliu15709732@user.noreply.gitee.com>
2023-06-18 12:26:29 +0800

    数字电路文章更新 && pdf组件优化

--- src/components/commonComponents/PDFcomponents.jsx
+++ src/components/commonComponents/PDFcomponents.jsx
@@ -4,11 +4,13 @@ const PDFPreview = ({ pdfUrl}) => {
   const iframeStyle = {
     width: `100%`,
     height: `65vh`,
+    marginTop:"45px",
+    marginBottom:"45px"
   };
 
   return (
     <div>
-      <iframe src={pdfUrl} style={iframeStyle} frameBorder="0"></iframe>
+      <iframe src={pdfUrl} style={iframeStyle} frameBorder="0" ></iframe>
     </div>
   );
 };
